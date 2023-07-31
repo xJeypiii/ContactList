79ef79651419d8e9d7b5e689b69dd2f49c900a77
jpjbriones <jpjbriones01@gmail.com>
2023-07-31 20:09:43 +0800

    database encryption

--- lib/src/main/java/com/codev/recruitment/lib/dagger/DatabaseModule.kt
+++ lib/src/main/java/com/codev/recruitment/lib/dagger/DatabaseModule.kt
@@ -2,6 +2,7 @@ package com.codev.recruitment.lib.dagger
 
 import android.content.Context
 import androidx.room.Room
+import com.codev.recruitment.lib.BuildConfig
 import com.codev.recruitment.lib.room.daos.ContactDao
 import com.codev.recruitment.lib.room.databases.AppDatabase
 import dagger.Module
@@ -9,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -21,10 +24,10 @@ class DatabaseModule {
 
     @Provides
     fun provideAppDatabase(@ApplicationContext appContext: Context): AppDatabase {
-        return Room.databaseBuilder(
-            appContext,
-            AppDatabase::class.java,
-            "app_database"
-        ).build()
+
+        val passphrase = SQLiteDatabase.getBytes(BuildConfig.PASSPHRASE.toCharArray())
+        return Room.databaseBuilder(appContext, AppDatabase::class.java, "app_database")
+            .openHelperFactory(SupportFactory(passphrase))
+            .build()
     }
 }
